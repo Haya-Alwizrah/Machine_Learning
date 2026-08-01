5e418197fe7da5157d972cf46380abb43579596e
HayaAlwizrah <hayaalwizrah@gmail.com>
2026-08-01 16:22:17 +0300

    Update app.py

--- Project/phase3_Interface/app.py
+++ Project/phase3_Interface/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, jsonify
-from Project.model.model import all_skills, get_recommendations
+from Project.phase2_Model.model import recommend_jobs
 
 app = Flask(__name__)
 
@@ -12,25 +12,17 @@ def home():
 def details():
     return render_template('details.html')
 
-@app.route('/try')
-def try_model():
-    return render_template('try.html', available_skills=all_skills)
+@app.route("/recommend", methods=["POST"])
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    data = request.json
-    user_skills = data.get('skills', [])
-    
-    if not user_skills:
-        return jsonify({"jobs": [], "missing_skills": []})
-        
-    results = get_recommendations(user_skills)
-    
+def recommend():
+
+    data = request.get_json()
+
+    skills = data["skills"]
+
+    results = recommend_jobs(skills)
+
     return jsonify(results)
-
-@app.route('/api/skills')
-def get_skills():
-    return jsonify(all_skills)
 
 if __name__ == '__main__':
     app.run(debug=True)
