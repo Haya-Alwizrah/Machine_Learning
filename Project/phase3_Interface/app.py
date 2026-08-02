877bb75586e8c3fc1d9fdcb82248ee312aa40a4a
HayaAlwizrah <hayaalwizrah@gmail.com>
2026-08-03 01:18:18 +0300

    Update app.py

--- Project/phase3_Interface/app.py
+++ Project/phase3_Interface/app.py
@@ -52,4 +52,7 @@ def get_skills():
 
 # ----------------------------------------
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
